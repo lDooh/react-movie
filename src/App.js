d9412ce878238d2445c2d453b576f8c652558012
IDooh <dlengml0528@gmail.com>
2023-03-26 21:11:11 +0900

    fix typo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Detail from "./routes/Detail";
 
 function App() {
     return (
-        <Router basename="{process.env.PUBLIC_URL}">
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/movie/:id"  element={<Detail />} />
                 <Route path="/" element={<Home />} />
